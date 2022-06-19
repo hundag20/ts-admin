933901d98ca9aa63100138cf7074da94576c955d
Hunda Guluma <hundaguluma@gmail.com>
2022-06-19 12:03:03 +0300

    reports charts done

--- includes/connect.php
+++ includes/connect.php
@@ -1,4 +1,4 @@
-<?-- php 
+<?php 
 		$link = mysqli_connect("127.0.0.1", "etmilestonecom_ts_user", "ts_password");
 		mysqli_select_db($link, "etmilestonecom_trvtps");
 		mysqli_query($link, "SET CHARACTER SET utf8");
